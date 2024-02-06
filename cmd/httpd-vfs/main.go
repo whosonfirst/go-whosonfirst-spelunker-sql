8eb19ce5ccb134e1c0f1513749f1e52a6790cf2b
thisisaaronland <devnull@localhost>
2024-02-05 17:58:28 -0800

    add ensureVFS to httpd-vfs

--- cmd/httpd-vfs/main.go
+++ cmd/httpd-vfs/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"flag"
+	"fmt"
 	"github.com/psanford/sqlite3vfs"
 	"github.com/psanford/sqlite3vfshttp"
 	"log/slog"
@@ -43,6 +43,49 @@ func (rt *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	return tr.RoundTrip(req)
 }
 
+func ensureVFS(spelunker_uri string) (bool, string, error) {
+
+	u, err := url.Parse(spelunker_uri)
+
+	if err != nil {
+		return false, spelunker_uri, err
+	}
+
+	if u.Host != "sqlite3" {
+		return false, spelunker_uri, nil
+	}
+
+	q := u.Query()
+
+	if !q.Has("vfs") {
+		return false, spelunker_uri, nil
+	}
+
+	vfs_url := q.Get("vfs")
+
+	vfs := sqlite3vfshttp.HttpVFS{
+		URL:          vfs_url,
+		RoundTripper: &roundTripper{
+			// referer:   *referer,
+			// userAgent: *userAgent,
+		},
+	}
+
+	err = sqlite3vfs.RegisterVFS("httpvfs", &vfs)
+
+	if err != nil {
+		return false, spelunker_uri, fmt.Errorf("Failed to register VFS", "error", err)
+	}
+
+	dsn := "not_a_real_name.db?vfs=httpvfs&mode=ro"
+	q.Set("dsn", dsn)
+	q.Del("vfs")
+
+	u.RawQuery = q.Encode()
+
+	return true, u.String(), nil
+}
+
 func main() {
 
 	ctx := context.Background()
@@ -57,54 +100,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	fs.VisitAll(func(fl *flag.Flag) {
+	changed, vfs_uri, err := ensureVFS(opts.SpelunkerURI)
 
-		if fl.Name != "spelunker-uri" {
-			return
-		}
+	if err != nil {
+		logger.Error("Failed to parse spelunker URI", "error", err)
+		os.Exit(1)
+	}
 
-		spelunker_uri := fl.Value.String()
-		u, err := url.Parse(spelunker_uri)
-
-		if err != nil {
-			slog.Error("Failed to parse spelunker URI", "error", err)
-			return
-		}
-
-		if u.Host != "sqlite3" {
-			return
-		}
-
-		q := u.Query()
-
-		if !q.Has("vfs") {
-			return
-		}
-
-		vfs_url := q.Get("vfs")
-
-		vfs := sqlite3vfshttp.HttpVFS{
-			URL:          vfs_url,
-			RoundTripper: &roundTripper{
-				// referer:   *referer,
-				// userAgent: *userAgent,
-			},
-		}
-
-		err = sqlite3vfs.RegisterVFS("httpvfs", &vfs)
-
-		if err != nil {
-			slog.Error("Failed to register VFS", "error", err)
-			return
-		}
-
-		dsn := "not_a_real_name.db?vfs=httpvfs&mode=ro"
-		q.Set("dsn", dsn)
-		q.Del("vfs")
-
-		u.RawQuery = q.Encode()
-		opts.SpelunkerURI = u.String()
-	})
+	if changed {
+		opts.SpelunkerURI = vfs_uri
+	}
 
 	err = server.RunWithOptions(ctx, opts, logger)
 
