b41f23d884bccc9975448e836a57a5211f8998d4
thisisaaronland <devnull@localhost>
2024-02-15 23:12:26 -0800

    Implement GetRecents; adjust to interface changes

--- spelunker_sql.go
+++ spelunker_sql.go
@@ -6,7 +6,8 @@ import (
 	"fmt"
 	_ "log/slog"
 	"net/url"
-
+	"time"
+	
 	"github.com/aaronland/go-pagination"
 	"github.com/whosonfirst/go-whosonfirst-spelunker"
 	wof_spr "github.com/whosonfirst/go-whosonfirst-spr/v2"
@@ -93,7 +94,7 @@ func (s *SQLSpelunker) getById(ctx context.Context, q string, args ...interface{
 	}
 }
 
-func (s *SQLSpelunker) GetDescendants(ctx context.Context, id int64, pg_opts pagination.Options) (wof_spr.StandardPlacesResults, pagination.Results, error) {
+func (s *SQLSpelunker) GetDescendants(ctx context.Context, pg_opts pagination.Options, id int64, filters ...spelunker.Filter) (wof_spr.StandardPlacesResults, pagination.Results, error) {
 
 	where := "instr(belongsto, ?) > 0"
 	return s.querySPR(ctx, pg_opts, where, id)
@@ -118,8 +119,18 @@ func (s *SQLSpelunker) CountDescendants(ctx context.Context, id int64) (int64, e
 	}
 }
 
-func (s *SQLSpelunker) Search(ctx context.Context, search_opts *spelunker.SearchOptions, pg_opts pagination.Options) (wof_spr.StandardPlacesResults, pagination.Results, error) {
+func (s *SQLSpelunker) Search(ctx context.Context, pg_opts pagination.Options, search_opts *spelunker.SearchOptions) (wof_spr.StandardPlacesResults, pagination.Results, error) {
 
 	where := "names_all MATCH ?"
 	return s.querySearch(ctx, pg_opts, where, search_opts.Query)
 }
+
+func (s *SQLSpelunker) GetRecent(ctx context.Context, pg_opts pagination.Options, d time.Duration, filters ...spelunker.Filter) (wof_spr.StandardPlacesResults, pagination.Results, error) {
+
+	now := time.Now()
+	then := now.Unix() - int64(d.Seconds())
+	
+	where := "lastmodified >= ? ORDER BY lastmodified DESC"
+	return s.querySPR(ctx, pg_opts, where, then)	
+}
+
